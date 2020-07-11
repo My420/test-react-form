7f632413db8ff3fadf9d7584247961cf3bea4190
My420 <my420V@gmail.com>
2020-07-11 17:36:09 +0300

    refactoring animation Switch component

--- src/components/Animation/Switch/Switch.js
+++ src/components/Animation/Switch/Switch.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition, SwitchTransition } from 'react-transition-group';
-import './animation.css';
+import styles from './Switch.module.scss';
+
+const animationClassNames = {
+  enter: styles.enter,
+  enterActive: styles.enterActive,
+  exit: styles.exit,
+  exitActive: styles.exitActive,
+};
 
 const SwitchAnimation = ({ children, isPlay }) => (
   <SwitchTransition mode="out-in">
     <CSSTransition
       key={isPlay}
       addEndListener={(node, done) => node.addEventListener('transitionend', done, false)}
-      classNames="switchAnimation"
+      classNames={animationClassNames}
     >
       <div>
-        <div className="animationContainer">{children}</div>
+        <div className={styles.container}>{children}</div>
       </div>
     </CSSTransition>
   </SwitchTransition>
